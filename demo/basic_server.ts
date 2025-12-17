539f6b69cf7a445defec4b219805d8b2200acbca
libz <46468889+SinuxLee@users.noreply.github.com>
2025-12-17 12:30:01 +0800

    Implement basic Express server with Socket.IO

---
+++ demo/basic_server.ts
@@ -0,0 +1,86 @@
+import express from 'express'
+import socketio from 'socket.io'
+import http from 'http'
+import cors from 'cors'
+import morgan from 'morgan'
+import bodyparser from 'body-parser'
+import path from 'path'
+import cookieparser from "cookie-parser"
+
+
+// 服务应该提供的基本功能
+interface Service{
+
+}
+
+// 应用的一般流程
+abstract class Application{
+
+}
+
+// 服务
+class Server extends Application implements Service{
+
+}
+
+class GateServer implements Server{
+   protected readonly expressApp : express.Application;
+   protected readonly webServer : http.Server;
+   protected readonly socketIO : socketio.Server;
+
+   public constructor(){
+      this.expressApp = express();
+      this.webServer = http.createServer(this.expressApp);
+      this.socketIO = socketio(this.webServer)
+
+      this.initApp();
+      this.initRouter();
+      this.initSocketIOEvent();
+   }
+
+   get socketio(): socketio.Server{
+      return this.socketIO;
+   }
+
+   initApp(){
+      // 跨域问题
+      this.expressApp.use(cors({origin: true, credentials: true}));
+      this.expressApp.enable('trust proxy');
+      this.expressApp.disable('etag');
+      this.expressApp.disable('x-powered-by');
+
+      // http访问日志
+      this.expressApp.use(morgan("combined"));
+      this.expressApp.use(bodyparser.json());
+      this.expressApp.use(bodyparser.urlencoded({extended: false}));
+      this.expressApp.use(cookieparser());
+
+      // web资源地址
+      this.expressApp.use(express.static(path.join(__dirname, 'public')));
+   }
+
+   initRouter(){
+      this.expressApp.get('/hello', function(req, res){
+         res.send({"txt":"hello world~"})
+      })
+   }
+
+   initSocketIOEvent(){
+      this.socketIO.on('connection', function (socket) {
+         socket.emit('news', { hello: 'world' });
+      
+         socket.on('msg', function (data) {
+           console.log(data);
+         });
+        });
+   }
+
+   run(){
+      this.webServer.listen(3000, function(){
+         console.log('app is runing!');
+      })
+   }
+}
+
+let gate = new GateServer();
+gate.run();
